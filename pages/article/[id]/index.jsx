800ad2d4ebbc3978d154f43eb25fbd12de056d26
CarlosHLeite <carlosleiteh@gmail.com>
2022-04-10 23:49:52 -0300

    done

--- pages/article/[id]/index.jsx
+++ pages/article/[id]/index.jsx
@@ -1,9 +1,49 @@
 
+import Link from 'next/link'
 
-const article = () => {
+
+
+const article = ({ article }) => {
+  // const router = useRouter()
+  // const { id } = router.query
+
   return (
-    <div>article</div>
+    <>
+     
+      <h1>{article.title}</h1>
+      <p>{article.body}</p>
+      <br />
+      <Link href='/'>Go Back</Link>
+    </>
   )
 }
+
+export const getStaticProps = async (context) => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`)
+ 
+  const article = await res.json()
+
+  return {
+    props: {
+      article,
+    },
+  }
+}
+
+export const getStaticPaths = async () => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts`)
+  
+  const articles = await res.json()
+
+  const ids = articles.map((article) => article.id)
+  const paths = ids.map((id) => ({ params: { id: id.toString() } }))
+
+  return {
+    paths,
+    fallback: false,
+  }
+}
+
+
 
 export default article
